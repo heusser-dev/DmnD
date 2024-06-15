b3f27c9ba87f989a6ff2bbe0f6b21c5688f7d726
heusser-dev <heusser.escamilla@gmail.com>
2024-06-14 22:22:09 -0400

    first

---
+++ src/components/Content/Card/card.tsx
@@ -0,0 +1,157 @@
+import React, { useRef } from 'react';
+import Modelo3d from '../modelo3d/modelo3d';
+import skills from './skills.png'; // Ajusta la ruta según la estructura de tu proyecto
+import Button from 'antd/es/button';
+import { FilePdfOutlined } from '@ant-design/icons';
+
+let classTitle: string = "text-3xl font-bold  mb-14 mt-14 sm:w-5/12 px-4 w-full animate-fade-in-right dark:text-slate-200   "
+let classTitle2: string = "text-3xl font-bold sm:ml-auto  mb-14 mt-14 px-4 mr-20 sm:w-5/12 animate-fade-in-right dark:text-slate-200 flex sm:justify-end  "
+
+let classContainerHover: string = "flex hover:scale-105 transform transition-transform duration-300"
+let classCardHover: string = "w-full flex md:w-7/12 md:pr-5 mb-24  px-6 py-6  bg-white dark:bg-slate-700 dark:bg-opacity-80 bg-opacity-80 rounded-lg animate-expand-vertically animate-delay-700"
+
+let classText: string = "text-lg dark:text-white "
+let classAnimatin: string = "flex flex-wrap  px-8 hover:scale-105 transform transition-transform duration-300 "
+let classAnimatin2: string = "flex flex-wrap px-8 justify-end hover:scale-105 transform transition-transform duration-300 "
+
+
+
+const ComponenteConModelo3D = () => {
+
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const playSound = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  }
+    
+  const dowloadPdf= ()=> {
+    const pdfFilePath = '/assets/CV-HeusserEscamilla.pdf';
+    const link = document.createElement('a');
+    link.href = pdfFilePath;
+    link.download = 'CV-Heusser-Escamilla.pdf';
+    link.click();
+    playSound();
+  }
+  return (
+
+   
+    <div className="container z-40 md:w-11/12 w-full h-auto  mb-4 mt-0 bg-white bg-opacity-0 rounded-lg">
+      <div className="w-full  ">
+        <Modelo3d />
+      </div>
+     
+      {/* <div className={classContainerHover}>
+
+        <h1 className="text-3xl font-bold mb-14 sm:w-5/12 mt-16 px-4 w-full animate-fade-in-right dark:text-slate-200   ">Oliver Dmnd</h1>
+      </div>
+      <div className={classAnimatin}>
+        <div className={classCardHover}>
+          <div>
+
+            <p className={classText + "mb-8"}>
+              Desarrollador Full Stack con experiencia en Angular, Redux, NestJS, AWS ,
+              ERP(odoo) y MongoDB, utilizando JavaScript y TypeScript para construir
+              aplicaciones web robustas. Mi enfoque proactivo y habilidades de comunicación
+              efectiva me permiten colaborar exitosamente en equipos, contribuyendo al
+              éxito de proyectos, ya sea con las tecnologías antes mencionadas o con otros
+              frameworks de JavaScript y Python, gracias a mi adaptabilidad en ámbitos de
+              programación.
+            </p>
+            <audio ref={audioRef}>
+        <source src="/assets/hero_simple-celebration-03.wav" type="audio/wav" />
+        Tu navegador no soporta el elemento de audio.
+      </audio>
+      
+        <Button type="dashed" onClick={dowloadPdf} icon={<FilePdfOutlined  />} size='large' className='dark:bg-white bg-custom-blue bg-opacity-30 mx-auto w-full  hover:scale-105 transform transition-transform duration-300'>
+        Descargar CV
+      </Button>
+          </div>
+        </div>
+
+        <img className=' h-60  w-auto ml-auto mr-auto hover:scale-125 transform transition-transform duration-300'  src='https://cdn-icons-png.flaticon.com/512/1488/1488581.png'></img>
+      
+      </div>
+
+
+      <div className={classContainerHover} >
+
+        <h1 className={classTitle2}>HABILIDADES</h1>
+      </div>
+      <div className={classAnimatin2}>
+      <img className=' h-60 w-auto mr-auto ml-auto mb-8 hover:scale-125 transform transition-transform duration-300' src={process.env.PUBLIC_URL + '/img/skills.png'} />
+       <div className={classCardHover}>
+          <p className={classText}>
+            Diseño de UI/UX: frameworks JavaScript, HTML5, CSS, Tailwind CSS y similares.
+            Desarrollo Back-End: TypeScript (NestJS, Express), Python (Django).
+            Desarrollo y consumo de API REST.
+            Servicios de AWS (S3, EC2, Glue).
+            Control de versiones con Git (GitHub).
+            Resolución de problemas, aprendizaje continuo, gestión del tiempo,
+            comunicación, resiliencia, empatía y creatividad.
+          </p>
+        </div>
+
+      </div>
+
+
+      <div className={classContainerHover}>
+
+        <h1 className={classTitle}>2023- HOY | DESARROLLO FULL STACKS JUNIOR | SAVIA (FRESH SPA)</h1>
+      </div>
+      <div className={classAnimatin}>
+        <div className={classCardHover}>
+          <p className={classText}>
+            Como desarrollador utilicé tecnologías como Angular, Ngrx, Ngx, Tailwind CSS,
+            NestJS, TypeScript y MongoDB para llevar el sistema de ventas y gestion interna
+            de la empresa a un nuevo nivel donde la integracion con un sistema de ERP como
+            Odoo eran vitales con estas habilidades logre aportar con la optimizacion y
+            creacion de diferentes funcionalidades que ayudan en la gestion de los procesos
+            de el negocio.
+          </p>
+        </div>
+        <img className=' h-60 w-auto mr-auto ml-auto mb-8 hover:scale-125 transform transition-transform duration-300' src={process.env.PUBLIC_URL + '/img/coding.png'} />
+
+      </div>
+      <div className={classContainerHover}>
+        <h1 className={classTitle2}>2022- 2023 | DESAROLLO FREE LANCE - PYTHON</h1>
+
+      </div>
+      <div className={classAnimatin2}>
+        <img className=' h-60 w-auto mr-auto ml-auto mb-8 hover:scale-125 transform transition-transform duration-300' src={process.env.PUBLIC_URL + '/img/freelance.png'} />
+        <div className={classCardHover}>
+          <p className={classText}>
+            Como desarrollador freelance en Django, he colaborado con particulares para
+            ofrecer soluciones web personalizadas y efectivas. Mis responsabilidades
+            incluyen el diseño, desarrollo e implementación de aplicaciones web adaptadas
+            a las necesidades individuales de los clientes.
+          </p>
+        </div>
+
+      </div>
+
+      <div className={classContainerHover}>
+
+        <h1 className={classTitle}>2020 - 2022 | WEB MASTER -EMPRENDIMIENTO</h1>
+      </div>
+      <div className={classAnimatin}>
+        <div className={classCardHover}>
+          <p className={classText}>
+            Diseñé, implementé y gestioné una tienda en línea basada en WooCommerce,
+            asegurando una experiencia de compra fluida y segura. Además, lideré
+            estrategias de redes sociales para aumentar la participación del usuario y la
+            visibilidad de la marca.
+          </p>
+        </div>
+        <div></div>
+        <img className=' h-60 w-auto mr-auto ml-auto mt-0 mb-8 hover:scale-125 transform transition-transform duration-300 ' src={process.env.PUBLIC_URL + '/img/cloud.png'} />
+
+      </div> */}
+
+
+    </div>
+  );
+};
+
+export default ComponenteConModelo3D;
---
+++ src/components/Content/modelo3d/modelo3d.jsx
@@ -0,0 +1,94 @@
+import { useRef, useEffect } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+const Modelo3d = () => {
+  const mountRef = useRef(null);
+
+  useEffect(() => {
+    // Data from the canvas
+    const currentRef = mountRef.current;
+    const { clientWidth: width, clientHeight: height } = currentRef;
+
+    // Scene, camera, renderer
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(25, width / height, 0.1, 100);
+    scene.add(camera);
+    camera.position.set(0,53,0);
+    camera.lookAt(new THREE.Vector3());
+
+    const renderer = new THREE.WebGLRenderer(({ alpha: true }));
+    renderer.setSize(width, height);
+    currentRef.appendChild(renderer.domElement);
+
+    // OrbitControls
+    const orbitControls = new OrbitControls(camera, renderer.domElement);
+    orbitControls.enableDamping = true;
+
+    // Resize canvas
+    const resize = () => {
+      renderer.setSize(currentRef.clientWidth, currentRef.clientHeight);
+      camera.aspect = currentRef.clientWidth / currentRef.clientHeight;
+      camera.updateProjectionMatrix();
+      console.log(camera)
+    };
+    window.addEventListener("resize", resize);
+
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load('./models/dmnd/dmnd1.gltf', (gltf) => {
+      const model = gltf.scene;
+      
+      model.scale.set(2.6, 2.6, 2.6); 
+      model.position.y = 0; // Ajusta la posición en el eje Y según sea necesario/ Ajusta los valores según sea necesario1
+      model.position.z = 0; 
+      model.position.x = 0; 
+
+      scene.add(model);
+      
+
+      // Set the initial rotation of the model
+      // model.rotation.y = Math.PI / 2; // Rotate 90 degrees initially
+      console.log(model)
+    });
+
+    // Animate the scene
+    const animate = () => {
+      orbitControls.update();
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+
+      // Rotate the model
+      if (scene.children.length > 0) {
+        const model = scene.children.find((child) => child.type === "Group");
+        if (model) {
+                    // model.rotation.y += 0.003;
+                    model.rotation.z -= 0.01; // Rotate 90 degrees initially
+
+                    // model.rotation.x += 0.02 ; // Adjust the rotation speed as needed
+                    // Adjust the rotation speed as needed
+        }
+      }
+    };
+    animate();
+
+    const ambientalLight = new THREE.AmbientLight(0xffffff, 4);
+    scene.add(ambientalLight);
+
+    return () => {
+      window.removeEventListener("resize", resize);
+      currentRef.removeChild(renderer.domElement);
+      console.log(camera)
+    };
+  }, []);
+
+  return (
+    <div>
+
+    <h2 className=" text-white text-2xl"></h2>
+    <div className='w-full h-96 ml-0 mr-0 mt-0 pt-0' ref={mountRef}></div>
+    </div>
+  );
+};
+
+export default Modelo3d;
